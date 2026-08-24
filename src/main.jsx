e5e48cf65dabe590d4673f0078297aeafeca48f0
subhankhan4427 <sk3354895@gmail.com>
2026-08-24 16:26:41 +0500

    bkjb

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-500.css";
+import "@fontsource/inter/latin-600.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
